3416162364bcd5350b8811b469825c0f8637f3b2
Charlydcn <Charly.ducournau@outlook.fr>
2023-03-15 16:59:02 +0100

    changed "genre" to "movieGenre" to avoid confusion

--- Movie.php
+++ Movie.php
@@ -3,17 +3,17 @@
     class Movie
     {
         private string $_title;
-        private string $_genre;
+        private string $_movieGenre;
         private DateTime $_releaseDate;
         private int $_duration;
         private Director $_director;
         private array $_casting;
         private string $_synopsis;
 
-        public function __construct(string $_title, string $_genre, string $_releaseDate, int $_duration, Director $_director,array $_casting, string $_synopsis)
+        public function __construct(string $_title, string $_movieGenre, string $_releaseDate, int $_duration, Director $_director,array $_casting, string $_synopsis)
         {
             $this->_title = $_title;
-            $this->_genre = $_genre;
+            $this->_movieGenre = $_movieGenre;
             $this->_releaseDate = new DateTime($_releaseDate);
             $this->_duration = $_duration;
             $this->_director = $_director;

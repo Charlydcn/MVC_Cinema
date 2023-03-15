aa736dd5bc1aef82d41595ddab0dc43c8400b9a9
Charlydcn <Charly.ducournau@outlook.fr>
2023-03-15 16:43:28 +0100

    fixed datatype: birthDate from "int" to "DateTime"

--- Director.php
+++ Director.php
@@ -5,14 +5,14 @@
             private string $_firstName;
             private string $_lastName;
             private $_genre;
-            private int $_birthDate;
+            private DateTime $_birthDate;
 
-            public function __construct(string $_firstName, string $_lastName, $_genre, $_birthDate)
+            public function __construct(string $_firstName, string $_lastName, string $_genre, string $_birthDate)
             {
                 $this->_firstName = $_firstName;
                 $this->_lastName = $_lastName;
                 $this->_genre = $_genre;
-                $this->_birthDate = $_birthDate;
+                $this->_birthDate = new DateTime($_birthDate);
             }
 
         }

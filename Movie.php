4a90036ae1b505a0887bbd272c1622f8db7c2d8b
Charlydcn <Charly.ducournau@outlook.fr>
2023-03-15 16:42:41 +0100

    added Movie property "casting"

--- Movie.php
+++ Movie.php
@@ -7,15 +7,17 @@
         private DateTime $_releaseDate;
         private int $_duration;
         private Director $_director;
+        private array $_casting;
         private string $_synopsis;
 
-        public function __construct($_title, $_genre, $_releaseDate, $_duration, $_director, $_synopsis)
+        public function __construct(string $_title, string $_genre, string $_releaseDate, int $_duration, Director $_director,array $_casting, string $_synopsis)
         {
             $this->_title = $_title;
             $this->_genre = $_genre;
             $this->_releaseDate = new DateTime($_releaseDate);
             $this->_duration = $_duration;
             $this->_director = $_director;
+            $this->_casting = [];
             // $this->_director->addDirector($this);
             $this->_synopsis = $_synopsis;
         }

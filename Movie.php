a5e95d2862d6cde32b257d0567ef2557dd1531b5
Charlydcn <charly.ducournau@outlook.fr>
2023-03-16 09:12:22 +0100

    added "actorMovies" property to class Actor

--- Movie.php
+++ Movie.php
@@ -10,15 +10,15 @@
         private array $_casting;
         private string $_synopsis;
 
-        public function __construct(string $_title, string $_movieGenre, string $_releaseDate, int $_duration, Director $_director,array $_casting, string $_synopsis)
+        public function __construct(string $_title, string $_movieGenre, string $_releaseDate, int $_duration, Director $_director, array $_casting, string $_synopsis)
         {
             $this->_title = $_title;
             $this->_movieGenre = $_movieGenre;
             $this->_releaseDate = new DateTime($_releaseDate);
             $this->_duration = $_duration;
             $this->_director = $_director;
-            $this->_casting = [];
             // $this->_director->addDirector($this);
+            $this->_casting = [];
             $this->_synopsis = $_synopsis;
         }
 

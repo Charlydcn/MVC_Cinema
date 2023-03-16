21f58881742f00bd83bd8f33712be99a3e479877
Charlydcn <charly.ducournau@outlook.fr>
2023-03-16 09:54:38 +0100

    changed $_movie to $_roleMovies

--- Role.php
+++ Role.php
@@ -4,13 +4,13 @@
         {
             private string $_firstName;
             private string $_lastName;
-            private array $_movie;
+            private array $_roleMovies;
 
             public function __construct(string $_firstName, string $_lastName)
             {
                 $this->_firstName = $_firstName;
                 $this->_lastName = $_lastName;
-                $this->_movie = [];
+                $this->_roleMovies = [];
             }
 
         }

8c4b59b53034899cac1efa196ed1ff358b91fc30
Charlydcn <charly.ducournau@outlook.fr>
2023-03-16 10:33:02 +0100

    added $_roleActors to class Role

--- Role.php
+++ Role.php
@@ -4,11 +4,13 @@
         {
             private string $_roleName;
             private array $_roleMovies;
+            private array $_roleActors;
 
             public function __construct(string $_roleName)
             {
                 $this->_roleName = $_roleName;
                 $this->_roleMovies = [];
+                $this->_roleActors = [];
             }
 
         }

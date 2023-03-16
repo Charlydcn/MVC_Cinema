c769e76636c6e9bd5cb42ce374df614deb93c152
Charlydcn <charly.ducournau@outlook.fr>
2023-03-16 08:59:53 +0100

    added property $_movies in class Director

--- Director.php
+++ Director.php
@@ -6,6 +6,7 @@
             private string $_lastName;
             private string $_genre;
             private DateTime $_birthDate;
+            private array $_movies;
 
             public function __construct(string $_firstName, string $_lastName, string $_genre, string $_birthDate)
             {
@@ -13,6 +14,7 @@
                 $this->_lastName = $_lastName;
                 $this->_genre = $_genre;
                 $this->_birthDate = new DateTime($_birthDate);
+                $this->_movies = $_movies;
             }
 
         }

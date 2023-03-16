a7a8ab5b22d981db14c5e467f02760ae2582e319
Charlydcn <charly.ducournau@outlook.fr>
2023-03-16 14:03:48 +0100

    wrote getters/setters + toString for class Role

--- Role.php
+++ Role.php
@@ -15,6 +15,62 @@
                 // $this->_role->addActors($this);
             }
 
+// ************************************************ MÉTHODES ************************************************ 
+// ************************************** ACCESSEURS (getters) *************************************
+
+            public function getRoleName()
+            {
+                return $this->_roleName;
+            }
+
+            public function getRoleMovies()
+            {
+                $result = "<ul>";
+                foreach ($this->_roleMovies as $movie) {
+                    $result .= "<li>" . $movie . "</li>";
+                }
+                $result .= "</ul>";
+                return $result;
+            }
+
+            public function getRoleActors()
+            {
+                $result = "<ul>";
+                foreach ($this->_roleActors as $actor) {
+                    $result .= "<li>" . $actor . "</li>";
+                }
+                $result .= "</ul>";
+                return $result;
+            }
+
+// *************************************************************************************************
+// ************************************** MUTATEURS (setters) ************************************** 
+
+            public function setRoleName($roleName)
+            {
+                $this->_roleName = $roleName;
+            }
+
+            public function setRoleMovies(array $roleMovies)
+            {
+                $this->_roleMovies = $roleMovies;
+            }
+
+            public function setRoleActors(array $roleActors)
+            {
+                $this->_roleActors = $roleActors;
+            }
+
+// *************************************************************************************************
+
+            public function __toString()
+            {
+                return "<strong>Role's name : </strong>" . $this->_roleName . "<br>"
+                . "<strong>Movies : </strong>". $this->getRoleMovies() . "<br>"
+                . "<strong>Actors who played the role : </strong>". $this->getRoleActors() . "<br>";
+
+            }
+
         }
 
 ?>

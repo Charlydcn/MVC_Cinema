76e1558c72bf6fe019f144d9812fe620b9e3b67a
Charlydcn <Charly.ducournau@outlook.fr>
2023-03-21 09:25:40 +0100

    added addMovie method to class Director (+Movie)

--- Director.php
+++ Director.php
@@ -1,48 +1,48 @@
-<?php 
+<?php
 
-    class Director extends Person
-        {
-            private array $_movies;
+class Director extends Person
+{
+    private array $_movies;
 
-            public function __construct(string $_firstName, string $_lastName, string $_genre, string $_birthDate, )
-            {
-                parent::__construct($_firstName, $_lastName, $_genre, $_birthDate);
-                $this->_movies = [];
-            }
+    public function __construct(string $_firstName, string $_lastName, string $_genre, string $_birthDate, )
+    {
+        parent::__construct($_firstName, $_lastName, $_genre, $_birthDate);
+        $this->_movies = [];
+    }
 
 
-// ************************************************ MÉTHODES ************************************************ 
+    // ************************************************ MÉTHODES ************************************************ 
 // ************************************** ACCESSEURS (getters) **************************************
 
-            public function getMovies() // A TESTER
-            {
-                $result = "<ul>";
-                foreach ($this->_movies as $movie) {
-                    $result .= "<li>" . $movie . "</li>";
-                }
-                $result .= "</ul>";
-                return $result;
-            }
+    public function getMovies() // A TESTER
+    {
+        $result = "<ul>";
+        foreach ($this->_movies as $movie) {
+            $result .= "<li>" . $movie . "</li>";
+        }
+        $result .= "</ul>";
+        return $result;
+    }
 
     // *************************************************************************************************
     // ************************************** MUTATEURS (setters) ************************************** 
-            
-            public function setMovies(array $movies) // A TESTER
-            {
-                $this->_movies = $movies;
-            }
+
+    public function setMovies(array $movies) // A TESTER
+    {
+        $this->_movies = $movies;
+    }
 
     // *************************************************************************************************
 
-            public function addMovie(Movie $movie)
-            {
-                $this->_movies[] = $movie;
-            }
-
-            public function __toString() // A TESTER
-            {
-                return $this->getFirstName() . " " . $this->getLastName() . "<br>";
-        }
+    public function addMovie(Movie $movie)
+    {
+        $this->_movies[] = $movie;
     }
+
+    public function __toString() // A TESTER
+    {
+        return $this->getFirstName() . " " . $this->getLastName();
+    }
+}
 
 ?>

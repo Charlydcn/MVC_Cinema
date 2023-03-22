7a7ec13f6e4740d8dadd5e388106a3a88818daf1
Charlydcn <Charly.ducournau@outlook.fr>
2023-03-22 08:45:47 +0100

    created futures sections of the website

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cinéma</title>
+    <link rel="stylesheet" href="/assets/style.css">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100&display=swap"
+        rel="stylesheet">
+</head>
+
+<body>
+
+    <header>
+
+        <h1>POO CINEMA</h1>
+
+        <nav>
+            <ul>
+                <a href="movies.php">
+                    <li>Films</li>
+                </a>
+                <a href="actors.php">
+                    <li>Acteurs</li>
+                </a>
+                <a href="directors.php">
+                    <li>Producteurs</li>
+                </a>
+                <a href="roles.php">
+                    <li>Rôles</li>
+                </a>
+            </ul>
+        </nav>
+
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>

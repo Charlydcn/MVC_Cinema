158a2f5eec3b2281afc0fa6a2c60b702f57a7bd2
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-20 16:13:40 +0200

    set up new files organisation

--- view/template.php
+++ view/template.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?= $title ?> </title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/CSS/style.css">    
+    <link rel="stylesheet" href="../public/css/style.css">    
 </head>
 
 <body>

d980dbe75bb717e67a15faedb3563dba0ec3864f
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-20 16:58:38 +0200

    (template)added bootstrap navbar (will be changed)

--- view/template.php
+++ view/template.php
@@ -7,35 +7,73 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?= $title ?> </title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../public/css/style.css">    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 </head>
 
 <body>
 
     <header>
-        <nav>
-            <ul>
-                <a href="index.php">
-                    <li>Home</li>
-                </a>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <!-- <a href="movies.php">Movies</a>
 
-                <a href="movies.php">
-                    <li>Movies</li>
-                </a>
+            <a href="actors.php">Actors</a>
 
-                <a href="actors.php">
-                    <li>Actors</li>
-                </a>
+            <a href="directors.php">Directors</a>
 
-                <a href="directors.php">
-                    <li>Directors</li>
-                </a>
+            <a href="genres.php">Genres</a>
+        </nav> -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-                <a href="genres.php">
-                    <li>Genres</li>
-                </a>
-            </ul>
-        </nav>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">
+                            Home
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            Link
+                        </a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                    </li>
+
+                    <ul class="dropdown-menu">
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                Action
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                Another action
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">
+                                Something else here
+                            </a>
+                        </li>
+                    </ul>
+
+                    <li class="nav-item">
+                        <a class="nav-link disabled">
+                            Disabled
+                        </a>
+                    </li>    
+            </div>
+
+    </nav>
+
     </header>
 
 
@@ -47,6 +85,8 @@
     <footer>
 
     </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
 </body>
 

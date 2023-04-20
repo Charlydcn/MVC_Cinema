e5c2632aaa66ad36964fd70203e554c1c5b51c30
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-20 13:45:25 +0200

    (view) moved views to appropriated folder

--- view/movies.php
+++ view/movies.php
@@ -5,28 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Actors</title>
+    <title>Movies</title>
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="/assets/style.css">
-
-    <!-- *************PHP REQUIRE************* -->
-    <?php require 'assets/classes/Person.php' ?>
-    <?php require 'assets/classes/Director.php' ?>
-    <?php require 'assets/classes/Role.php' ?>
-    <?php require 'assets/classes/Actor.php' ?>
-    <?php require 'assets/classes/Movie.php' ?>
-    <?php require 'assets/classes/Casting.php' ?>
-    <?php require 'assets/classes/MovieGenre.php' ?>
-    <?php require 'assets/classes/indexold.php'?>
-    <style> <?php include 'assets/CSS/style.css'; ?> </style>
-    <!-- ************************************* -->
-
+    <link rel="stylesheet" href="/assets/CSS/style.css">
 </head>
 
 <body>
-
+    
     <header>
 
         <h1>POO CINEMA</h1>
@@ -55,9 +42,41 @@
             </ul>
         </nav>
 
-    </header>
+     </header>
 
     <main>
+        
+        <div id="movie_cards">
+
+            <article class="movie_card">
+                
+            </article>
+
+            <article class="movie_card">
+
+            </article>
+
+            <article class="movie_card">
+
+            </article>
+
+            <article class="movie_card">
+
+            </article>
+
+            <article class="movie_card">
+
+            </article>
+
+            <article class="movie_card">
+
+            </article>
+
+            <article class="movie_card">
+
+            </article>
+    
+        </div>
 
     </main>
 

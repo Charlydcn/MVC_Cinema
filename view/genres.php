86788599443118ee71db98edb822e6b100d4362e
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-21 11:42:37 +0200

    (view/movie) displayed all genre in list style

--- view/genres.php
+++ view/genres.php
@@ -4,7 +4,22 @@ ob_start();
 
 ?>
 
+<?php
+foreach ($sql->fetchAll() as $genre) {
+?>
 
+    <ul>
+        <li class="list-inline-item">
+            <a class="text-decoration-none text-reset fs-3" href="index.php?action=genre_detail&id=<?= $genre['id_movie_genre'] ?>">
+                <?= $genre['genre_name'] ?>
+
+            </a>
+
+            (<?= $genre['count'] ?>)
+        </li>
+    </ul>
+
+<?php } ?>
 
 <?php
 

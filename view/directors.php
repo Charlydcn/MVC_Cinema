3dd97d04132d25b9da6225cad962ecd4a00cc2a6
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-21 11:31:28 +0200

    (view/director) displayed all directors in cards

--- view/directors.php
+++ view/directors.php
@@ -4,7 +4,26 @@ ob_start();
 
 ?>
 
+<div class="row">
 
+    <?php
+
+    foreach ($sql->fetchAll() as $director) { ?>
+
+        <div class="col-lg-2">
+            <a class="text-decoration-none text-reset" href="">
+                <figure>
+                    <img src="public/img/<?= $director['portrait'] ?>" alt="portrait <?= $director['first_name'] . " " . $director['last_name'] ?>" class="rounded-circle img-thumbnail">
+                    <figcaption class="text-center fw-semibold"><?= $director['first_name'] . " " . $director['last_name'] ?></figcaption>
+                </figure>
+            </a>
+
+        </div>
+
+
+    <?php } ?>
+
+</div>
 
 <?php
 

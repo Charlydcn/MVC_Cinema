ea8c4c95cad400c20df2ed1b0d64b92353e49637
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-21 10:52:47 +0200

    (router) created all needed switch cases

--- index.php
+++ index.php
@@ -8,18 +8,75 @@ spl_autoload_register(function ($class_name) {
 
 $ctrlCinema = new CinemaController();
 
-if(isset($_GET['action'])) {
-    switch($_GET['action']) {
+$id = (isset($_GET['id'])) ? $_GET['id'] : null;
 
-        case "listMovies" : $ctrlCinema->listMovies();
-        
-        break;
+if (isset($_GET['action'])) {
+    switch ($_GET['action']) {
 
-        case "listActors" : $ctrlCinema->listActors();
-        
-        break;
+        // MOVIES **************************************************
+
+        case "movies":
+
+            $ctrlCinema->listMovies();
+
+            break;
+
+
+        case "movie_detail":
+
+            $ctrlCinema->movieDetail($id);
+            $ctrlCinema->movieCasting($id);
+
+            break;
+
+        // *********************************************************
+        // ACTORS **************************************************
+
+        case "actors":
+
+            $ctrlCinema->listActors();
+
+            break;
+
+
+        case "actor_detail":
+
+            $ctrlCinema->actorDetail($id);
+            $ctrlCinema->actorMovies($id);
+
+        // *********************************************************
+        // DIRECTORS **************************************************
+
+        case "directors":
+
+            $ctrlCinema->listDirectors();
+
+            break;
+
+
+        case "director_detail":
+
+            $ctrlCinema->directorDetail($id);
+
+            break;
+
+        // *********************************************************
+        // GENRE **************************************************
+
+        case "genres":
+
+            $ctrlCinema->listGenres();
+
+            break;
+
+
+        case "genre_detail":
+            
+            $ctrlCinema->genreDetail($id);
+
+            break;
+
+        // *********************************************************
 
     }
 }
-
-?>

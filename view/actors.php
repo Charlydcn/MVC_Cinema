5213b272bc46dd5722c0a73498a6fa83978c6f69
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-21 11:31:13 +0200

    (view/actor) displayed all actors in cards style

--- view/actors.php
+++ view/actors.php
@@ -4,8 +4,26 @@ ob_start();
 
 ?>
 
+<div class="row">
+
+    <?php
+
+    foreach ($sql->fetchAll() as $actor) { ?>
+
+        <div class="col-lg-2">
+            <a class="text-decoration-none text-reset" href="">
+                <figure>
+                    <img src="public/img/<?= $actor['portrait'] ?>" alt="portrait <?= $actor['first_name'] . " " . $actor['last_name'] ?>" class="rounded-circle img-thumbnail">
+                    <figcaption class="text-center fw-semibold"><?= $actor['first_name'] . " " . $actor['last_name'] ?></figcaption>
+                </figure>
+            </a>
+
+        </div>
 
 
+    <?php } ?>
+
+</div>
 <?php
 
 $content = ob_get_clean();

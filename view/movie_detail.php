03c4bcf04b6522237e6e4a8934eee6a67a22b354
Charlydcn <charly.ducournau@outlook.fr>
2023-04-24 09:52:43 +0200

    (view/movie_detail) changed view style

--- view/movie_detail.php
+++ view/movie_detail.php
@@ -12,35 +12,44 @@ $movieDetail = $sqlDetail->fetch();
 
 ?>
 
-<div class="row featurette">
-    <div class="col-md-7 order-md-2">
 
-        <p class="fs-3 fw-semibold"><?= $movieDetail['title'] ?> (<?= $movieDetail['release_date'] ?>)</p>
+<div class="card mb-3" style="max-width: 1024px">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="public/img/<?= $movieDetail['poster'] ?>" alt="portrait <?= $movieDetail['title'] ?>" class="img-fluid rounded-start">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h2 class="card-title"><?= $movieDetail['title'] ?> (<?= $movieDetail['release_date'] ?>)</h2>
+                <br>
+                <p class="card-text fw-bold lh-1">Genre(s) :
+                <p><?= $movieDetail['genres'] ?></p>
 
-        <p class="fw-bold lh-1">Genre(s) :
-        <p><?= $movieDetail['genres'] ?></p>
+                <p class="card-text fw-bold lh-1">Duration :
+                <p><?= $movieDetail['length'] ?></p>
 
-        <p class="fw-bold lh-1">Duration :
-        <p><?= $movieDetail['length'] ?></p>
+                <p class="card-text fw-bold lh-1">Director : </p>
+                <p><a class="text-decoration-none text-reset" href="index.php?action=director_detail&id=<?=$movieDetail['id_director']?>"><?= $movieDetail['first_name'] . " " . $movieDetail['last_name'] ?></a></p>
 
-        <p class="fw-bold lh-1">Director : </p>
-        <p><?= $movieDetail['first_name'] . " " . $movieDetail['last_name'] ?></p>
+                <p class="card-text fw-bold">Synopsis :</p>
+                <p><?= $movieDetail['synopsis'] ?></p>
 
-        <p class="fw-bold">Synopsis :</p>
-        <p><?= $movieDetail['synopsis'] ?></p>
+                <p class="card-text fw-bold lh-1">Rating :</p>
+                <p><?= $movieDetail['rating'] ?>/5</p>
 
-        <p class="fw-bold lh-1">Rating :</p>
-        <p><?= $movieDetail['rating'] ?>/5</p>
-
+                <p class="card-text">
+                    <small>
+                        <a class="text-decoration-none" href="">
+                            <i class="fa-solid fa-pen-to-square"></i>    
+                        </a>
+                </small>
+                </p>
+            </div>
+        </div>
     </div>
-
-    <figure class="col-md-5 order-md-1">
-        <img src="public/img/<?= $movieDetail['poster'] ?>" alt="portrait <?= $movieDetail['title'] ?>" class="img-thumbnail">
-    </figure>
-
 </div>
 
-<div>
+<p class="fs-4">Casting : </p>
 
     <?php
 
@@ -49,16 +58,16 @@ $movieDetail = $sqlDetail->fetch();
 
 
         <ul>
-            <a href="index.php?action=actor_detail&id=<?= $casting['id_actor'] ?>">
-                <li><?= $casting['first_name'] . " " . $casting['last_name'] . " " . $casting['role_name'] ?></li>
+            <a class="text-decoration-none text-reset" href="index.php?action=actor_detail&id=<?= $casting['id_actor'] ?>">
+                <li><span class="fw-bold"><?= $casting['first_name'] . " " . $casting['last_name'] . "</span> (" . $casting['role_name'] . ") " ?></li>
             </a>
         </ul>
 
 </div>
 
-<?php
+<?php } ?>
 
-    }
+<?php
 
     $content = ob_get_clean();
     $title = $movieDetail['title'];

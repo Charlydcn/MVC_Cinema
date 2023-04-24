8168bf50b79c4124419c9c7afcbba64ee177c091
Charlydcn <charly.ducournau@outlook.fr>
2023-04-24 17:07:24 +0200

    (db) saved db

--- misc/save db/save_cinema_db.sql
+++ misc/save db/save_cinema_db.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `actor` (
   CONSTRAINT `actor_ibfk_1` FOREIGN KEY (`id_person`) REFERENCES `person` (`id_person`)
 ) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table cinema.actor : ~37 rows (environ)
+-- Listage des données de la table cinema.actor : ~8 rows (environ)
 INSERT INTO `actor` (`id_actor`, `id_person`) VALUES
 	(1, 2),
 	(2, 3),
@@ -161,9 +161,9 @@ CREATE TABLE IF NOT EXISTS `movie` (
 
 -- Listage des données de la table cinema.movie : ~7 rows (environ)
 INSERT INTO `movie` (`id_movie`, `title`, `release_date`, `length`, `synopsis`, `rating`, `poster`, `id_director`) VALUES
-	(1, 'Batman', '1989-09-13', 125, 'The Dark Knight of Gotham City begins his war on crime with his first major enemy being Jack Napier, a criminal who becomes the clownishly homicidal Joker.', 4, 'missing_poster.png', 1),
-	(2, 'Spider-man', '2002-06-12', 121, 'After being bitten by a genetically-modified spider, a shy teenager gains spider-like abilities that he uses to fight injustice as a masked superhero and face a vengeful enemy.', 3, 'missing_poster.png', 2),
-	(3, 'Skyfall', '2012-10-26', 143, 'James Bond\'s loyalty to M is tested when her past comes back to haunt her. When MI6 comes under attack, 007 must track down and destroy the threat, no matter how personal the cost.', 4, 'missing_poster.png', 3),
+	(1, 'Batman', '1989-09-13', 125, 'The Dark Knight of Gotham City begins his war on crime with his first major enemy being Jack Napier, a criminal who becomes the clownishly homicidal Joker.', 4, 'batman_1989.jpg', 1),
+	(2, 'Spider-man', '2002-06-12', 121, 'After being bitten by a genetically-modified spider, a shy teenager gains spider-like abilities that he uses to fight injustice as a masked superhero and face a vengeful enemy.', 3, 'spiderman_2002.jpg', 2),
+	(3, 'Skyfall', '2012-10-26', 143, 'James Bond\'s loyalty to M is tested when her past comes back to haunt her. When MI6 comes under attack, 007 must track down and destroy the threat, no matter how personal the cost.', 4, 'skyfall_2012.jpg', 3),
 	(4, 'Inception', '2010-07-16', 148, 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', 4.5, 'missing_poster.png', 4),
 	(5, 'The Shawshank Redemption', '1994-09-10', 142, 'Over the course of several years, two convicts form a friendship, seeking consolation and, eventually, redemption through basic compassion.', 5, 'missing_poster.png', 5),
 	(6, 'Pulp Fiction', '1994-10-14', 154, 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.', 4.5, 'missing_poster.png', 6),
@@ -201,52 +201,52 @@ CREATE TABLE IF NOT EXISTS `person` (
   PRIMARY KEY (`id_person`)
 ) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table cinema.person : ~44 rows (environ)
+-- Listage des données de la table cinema.person : ~0 rows (environ)
 INSERT INTO `person` (`id_person`, `first_name`, `last_name`, `birthdate`, `genre`, `portrait`) VALUES
-	(1, 'Tim', 'Burton', '1959-08-25', 'Male', 'missing_portrait.png'),
-	(2, 'Michael', 'Keaton', '1953-12-09', 'Male', 'missing_portrait.png'),
-	(3, 'Kim', 'Basinger', '1937-04-22', 'Female', 'missing_portrait.png'),
-	(5, 'Jack', 'Nicholson', '1937-04-22', 'Male', 'missing_portrait.png'),
-	(6, 'Jack', 'Palance', '1919-02-18', 'Male', 'missing_portrait.png'),
-	(7, 'Michael', 'Gough', '1916-12-23', 'Male', 'missing_portrait.png'),
-	(8, 'Tobey', 'Maguire', '1975-06-27', 'Male', 'missing_portrait.png'),
-	(9, 'Willem', 'Dafoe', '1955-07-22', 'Male', 'missing_portrait.png'),
-	(10, 'Kirsten', 'Dunst', '1982-04-30', 'Female', 'missing_portrait.png'),
-	(11, 'James', 'Franco', '1978-04-19', 'Male', 'missing_portrait.png'),
-	(12, 'Rosemary', 'Harris', '1927-09-19', 'Female', 'missing_portrait.png'),
-	(20, 'Sam', 'Raimi', '1959-10-23', 'Male', 'missing_portrait.png'),
-	(21, 'Sam', 'Mendes', '1965-08-01', 'Male', 'missing_portrait.png'),
-	(23, 'Daniel', 'Craig', '1968-03-02', 'Male', 'missing_portrait.png'),
-	(24, 'Judi', 'Dench', '1934-12-09', 'Female', 'missing_portrait.png'),
-	(25, 'Javier', 'Bardem', '1969-03-01', 'Male', 'missing_portrait.png'),
-	(26, 'Ralph', 'Fiennes', '1962-12-22', 'Male', 'missing_portrait.png'),
-	(27, 'Naomie', 'Harris', '1976-09-06', 'Female', 'missing_portrait.png'),
-	(28, 'Christopher', 'Nolan', '1970-07-30', 'Male', 'missing_portrait.png'),
-	(29, 'Leonardo', 'DiCaprio', '1974-11-11', 'Male', 'missing_portrait.png'),
-	(30, 'Ellen', 'Page', '1987-02-21', 'Female', 'missing_portrait.png'),
-	(31, 'Marion', 'Cotillard', '1975-09-30', 'Female', 'missing_portrait.png'),
-	(32, 'Tom', 'Hardy', '1977-09-15', 'Male', 'missing_portrait.png'),
-	(33, 'Ken', 'Watanabe', '1959-10-21', 'Male', 'missing_portrait.png'),
-	(34, 'Frank', 'Drabont', '1959-01-28', 'Male', 'missing_portrait.png'),
-	(35, 'Tim', 'Robbins', '1958-10-16', 'Male', 'missing_portrait.png'),
-	(36, 'Morgan', 'Freeman', '1937-06-01', 'Male', 'missing_portrait.png'),
-	(37, 'Clancy', 'Brown', '1959-01-05', 'Male', 'missing_portrait.png'),
-	(38, 'Bob', 'Gunton', '1945-11-15', 'Male', 'missing_portrait.png'),
-	(39, 'William', 'Sadler', '1950-04-13', 'Male', 'missing_portrait.png'),
-	(40, 'John', 'Travolta', '1954-02-18', 'Male', 'missing_portrait.png'),
-	(41, 'Samuel', 'L. Jackson', '1948-12-21', 'Male', 'missing_portrait.png'),
-	(42, 'Uma', 'Thurman', '1970-04-29', 'Female', 'missing_portrait.png'),
-	(43, 'Bruce', 'Willis', '1955-03-19', 'Male', 'missing_portrait.png'),
-	(44, 'Ving', 'Rhames', '1959-05-12', 'Male', 'missing_portrait.png'),
-	(45, 'Quentin', 'Tarantino', '1963-03-27', 'Male', 'missing_portrait.png'),
-	(46, 'Robert', 'Zemeckis', '1951-05-14', 'Male', 'missing_portrait.png'),
-	(47, 'Tom', 'Hanks', '1959-07-09', 'Male', 'missing_portrait.png'),
-	(48, 'Gary', 'Sinise', '1955-03-17', 'Male', 'missing_portrait.png'),
-	(49, 'Robin', 'Wright', '1966-04-08', 'Female', 'missing_portrait.png'),
-	(50, 'Mykelti', 'Williamson', '1960-03-04', 'Male', 'missing_portrait.png'),
-	(51, 'Sally', 'Field', '1946-11-06', 'Female', 'missing_portrait.png'),
-	(52, 'Monsieur', 'Test', '2023-04-20', 'Male', 'missing_portrait.png'),
-	(53, 'Madame', 'Test', '2023-04-20', 'Female', 'missing_portrait.png');
+	(1, 'Tim', 'Burton', '1959-08-25', 'Male', 'leonardo_dicaprio.jpg'),
+	(2, 'Michael', 'Keaton', '1953-12-09', 'Male', 'leonardo_dicaprio.jpg'),
+	(3, 'Kim', 'Basinger', '1937-04-22', 'Female', 'leonardo_dicaprio.jpg'),
+	(5, 'Jack', 'Nicholson', '1937-04-22', 'Male', 'leonardo_dicaprio.jpg'),
+	(6, 'Jack', 'Palance', '1919-02-18', 'Male', 'leonardo_dicaprio.jpg'),
+	(7, 'Michael', 'Gough', '1916-12-23', 'Male', 'leonardo_dicaprio.jpg'),
+	(8, 'Tobey', 'Maguire', '1975-06-27', 'Male', 'leonardo_dicaprio.jpg'),
+	(9, 'Willem', 'Dafoe', '1955-07-22', 'Male', 'leonardo_dicaprio.jpg'),
+	(10, 'Kirsten', 'Dunst', '1982-04-30', 'Female', 'leonardo_dicaprio.jpg'),
+	(11, 'James', 'Franco', '1978-04-19', 'Male', 'leonardo_dicaprio.jpg'),
+	(12, 'Rosemary', 'Harris', '1927-09-19', 'Female', 'leonardo_dicaprio.jpg'),
+	(20, 'Sam', 'Raimi', '1959-10-23', 'Male', 'leonardo_dicaprio.jpg'),
+	(21, 'Sam', 'Mendes', '1965-08-01', 'Male', 'leonardo_dicaprio.jpg'),
+	(23, 'Daniel', 'Craig', '1968-03-02', 'Male', 'leonardo_dicaprio.jpg'),
+	(24, 'Judi', 'Dench', '1934-12-09', 'Female', 'leonardo_dicaprio.jpg'),
+	(25, 'Javier', 'Bardem', '1969-03-01', 'Male', 'leonardo_dicaprio.jpg'),
+	(26, 'Ralph', 'Fiennes', '1962-12-22', 'Male', 'leonardo_dicaprio.jpg'),
+	(27, 'Naomie', 'Harris', '1976-09-06', 'Female', 'leonardo_dicaprio.jpg'),
+	(28, 'Christopher', 'Nolan', '1970-07-30', 'Male', 'leonardo_dicaprio.jpg'),
+	(29, 'Leonardo', 'DiCaprio', '1974-11-11', 'Male', 'leonardo_dicaprio.jpg'),
+	(30, 'Ellen', 'Page', '1987-02-21', 'Female', 'leonardo_dicaprio.jpg'),
+	(31, 'Marion', 'Cotillard', '1975-09-30', 'Female', 'leonardo_dicaprio.jpg'),
+	(32, 'Tom', 'Hardy', '1977-09-15', 'Male', 'leonardo_dicaprio.jpg'),
+	(33, 'Ken', 'Watanabe', '1959-10-21', 'Male', 'leonardo_dicaprio.jpg'),
+	(34, 'Frank', 'Drabont', '1959-01-28', 'Male', 'leonardo_dicaprio.jpg'),
+	(35, 'Tim', 'Robbins', '1958-10-16', 'Male', 'leonardo_dicaprio.jpg'),
+	(36, 'Morgan', 'Freeman', '1937-06-01', 'Male', 'leonardo_dicaprio.jpg'),
+	(37, 'Clancy', 'Brown', '1959-01-05', 'Male', 'leonardo_dicaprio.jpg'),
+	(38, 'Bob', 'Gunton', '1945-11-15', 'Male', 'leonardo_dicaprio.jpg'),
+	(39, 'William', 'Sadler', '1950-04-13', 'Male', 'leonardo_dicaprio.jpg'),
+	(40, 'John', 'Travolta', '1954-02-18', 'Male', 'leonardo_dicaprio.jpg'),
+	(41, 'Samuel', 'L. Jackson', '1948-12-21', 'Male', 'leonardo_dicaprio.jpg'),
+	(42, 'Uma', 'Thurman', '1970-04-29', 'Female', 'leonardo_dicaprio.jpg'),
+	(43, 'Bruce', 'Willis', '1955-03-19', 'Male', 'leonardo_dicaprio.jpg'),
+	(44, 'Ving', 'Rhames', '1959-05-12', 'Male', 'leonardo_dicaprio.jpg'),
+	(45, 'Quentin', 'Tarantino', '1963-03-27', 'Male', 'leonardo_dicaprio.jpg'),
+	(46, 'Robert', 'Zemeckis', '1951-05-14', 'Male', 'leonardo_dicaprio.jpg'),
+	(47, 'Tom', 'Hanks', '1959-07-09', 'Male', 'leonardo_dicaprio.jpg'),
+	(48, 'Gary', 'Sinise', '1955-03-17', 'Male', 'leonardo_dicaprio.jpg'),
+	(49, 'Robin', 'Wright', '1966-04-08', 'Female', 'leonardo_dicaprio.jpg'),
+	(50, 'Mykelti', 'Williamson', '1960-03-04', 'Male', 'leonardo_dicaprio.jpg'),
+	(51, 'Sally', 'Field', '1946-11-06', 'Female', 'leonardo_dicaprio.jpg'),
+	(52, 'Monsieur', 'Test', '2023-04-20', 'Male', 'leonardo_dicaprio.jpg'),
+	(53, 'Madame', 'Test', '2023-04-20', 'Female', 'leonardo_dicaprio.jpg');
 
 -- Listage de la structure de table cinema. role
 CREATE TABLE IF NOT EXISTS `role` (
@@ -255,7 +255,7 @@ CREATE TABLE IF NOT EXISTS `role` (
   PRIMARY KEY (`id_role`)
 ) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Listage des données de la table cinema.role : ~37 rows (environ)
+-- Listage des données de la table cinema.role : ~0 rows (environ)
 INSERT INTO `role` (`id_role`, `role_name`) VALUES
 	(1, 'Batman'),
 	(2, 'Vicki Vale'),

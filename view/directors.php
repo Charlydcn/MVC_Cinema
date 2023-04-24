a67cfa0ebb150387b9301d7877678f9297e38ebe
Charlydcn <charly.ducournau@outlook.fr>
2023-04-24 10:11:23 +0200

    (v/directors) added href to director_detail

--- view/directors.php
+++ view/directors.php
@@ -11,7 +11,7 @@ ob_start();
     foreach ($sql->fetchAll() as $director) { ?>
 
         <div class="col-lg-2">
-            <a class="text-decoration-none text-reset" href="">
+            <a class="text-decoration-none text-reset" href="index.php?action=director_detail&id=<?=$director['id_director']?>">
                 <figure>
                     <img src="public/img/<?= $director['portrait'] ?>" alt="portrait <?= $director['first_name'] . " " . $director['last_name'] ?>" class="rounded-circle img-thumbnail">
                     <figcaption class="text-center fw-semibold"><?= $director['first_name'] . " " . $director['last_name'] ?></figcaption>

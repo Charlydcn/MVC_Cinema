5d89a19c9da6d931e3a30bf198209ead57f2c288
Charlydcn <charly.ducournau@outlook.fr>
2023-04-24 14:10:44 +0200

    (v/ac_detail) added edit button href to dashboard

--- view/actor_detail.php
+++ view/actor_detail.php
@@ -4,7 +4,7 @@ ob_start();
 
 ?>
 
-<?php 
+<?php
 
 $actorDetail = $sqlDetail->fetch();
 $actorMovies = $sqlMovies->fetchAll();
@@ -15,28 +15,28 @@ $actorMovies = $sqlMovies->fetchAll();
   <div class="row g-0">
 
     <div class="col-md-4">
-        <img src="public/img/<?= $actorDetail['portrait'] ?>" alt="portrait <?= $actor['first_name'] . " " . $actor['last_name']?>" class="img-fluid rounded-start">
+      <img src="public/img/<?= $actorDetail['portrait'] ?>" alt="portrait <?= $actorDetail['first_name'] . " " . $actorDetail['last_name'] ?>" class="img-fluid rounded-start">
     </div>
 
     <div class="col-md-8">
       <div class="card-body">
-        
+
         <h2 class="card-title">
-            <?= $actorDetail['first_name'] . " " . $actorDetail['last_name'] ?>
+          <?= $actorDetail['first_name'] . " " . $actorDetail['last_name'] ?>
         </h2>
 
         <p class="card-text fw-bold lh-1">Birth-date :</p>
-        <p><?= $actorDetail['birthdate']?></p>
+        <p><?= $actorDetail['birthdate'] ?></p>
 
         <p class="card-text fw-bold lh-1">Genre :</p>
         <p><?= $actorDetail['genre'] ?></p>
 
         <p class="card-text">
-            <small>
-                <a class="text-decoration-none" href="">
-                    <i class="fa-solid fa-pen-to-square"></i>    
-                </a>
-            </small>
+          <small>
+            <a class="text-decoration-none" href="index.php?action=person_dashboard&id=<?= $actorDetail['id_person'] ?>">
+              <i class="fa-solid fa-pen-to-square"></i>
+            </a>
+          </small>
         </p>
 
       </div>
@@ -48,12 +48,12 @@ $actorMovies = $sqlMovies->fetchAll();
 
 <?php
 
-foreach($actorMovies as $movie) {
+foreach ($actorMovies as $movie) {
 
 ?>
 
-<p class="lh-1"><a class="text-decoration-none text-reset fw-bold" href="index.php?action=movie_detail&id=<?=$movie['id_movie']?>"><?= $movie['title']?></a><?=" (" . $movie['release_date'] . ")"?></p>
-<p><span class="fw-bold">Role : </span><?= $movie['role_name'] ?></p>
+  <p class="lh-1"><a class="text-decoration-none text-reset fw-bold" href="index.php?action=movie_detail&id=<?= $movie['id_movie'] ?>"><?= $movie['title'] ?></a><?= " (" . $movie['release_date'] . ")" ?></p>
+  <p><span class="fw-bold">Role : </span><?= $movie['role_name'] ?></p>
 
 <?php } ?>
 

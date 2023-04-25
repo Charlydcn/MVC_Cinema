c0a48336e12322cbb025d1d06418d9df416cfd31
Charlydcn <Charly.ducournau@outlook.fr>
2023-04-25 16:18:01 +0200

    moved form submit handling code to controller

--- index.php
+++ index.php
@@ -106,76 +106,8 @@ if (isset($_GET['action'])) {
             break;
 
         case "createPerson":
-            if (isset($_POST['submit'])) {
-                $first_name = filter_input(INPUT_POST, "first_name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-                $last_name = filter_input(INPUT_POST, "last_name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-                $birthdate = filter_input(INPUT_POST, "birthdate");
-                $genre = filter_input(INPUT_POST, "genre");
-                $isActor = filter_input(INPUT_POST, "isActor", FILTER_VALIDATE_BOOL);
-                $isDirector = filter_input(INPUT_POST, "isDirector", FILTER_VALIDATE_BOOL);
 
-
-                //************************ IMAGE *********************************
-
-                if (isset($_FILES['portrait'])) {
-                    $imgTmpName = $_FILES['portrait']['tmp_name'];
-                    $imgName = $_FILES['portrait']['name'];
-                    $imgSize = $_FILES['portrait']['size'];
-                    $imgError = $_FILES['portrait']['error'];
-
-                    $tabExtension = explode('.', $imgName);
-                    $extension = strtolower(end($tabExtension));
-
-                    //Tableau des extensions que l'on accepte
-                    $extensions = ['jpg', 'png', 'jpeg', 'gif'];
-                    $maxSize = 5000000;
-
-                    if (in_array($extension, $extensions) && $imgSize <= $maxSize && $imgError == 0) {
-                        $uniqueName = uniqid('', true); // uniqid génère un ID random (exemple 5f586bf96dcd38.73540086)
-                        $portrait = $uniqueName . '.' . $extension;
-                        move_uploaded_file($imgTmpName, 'public/img/' . $portrait);
-                    }
-                }
-                //****************************************************************
-
-                if ($isActor === null) {
-                    $isActor = false;
-                }
-
-                if ($isDirector === null) {
-                    $isDirector = false;
-                }
-
-
-                if ($first_name && $last_name && $birthdate && $genre && is_bool($isActor) && is_bool($isDirector)) {
-
-                    if ($isActor === true || $isDirector === true) {
-
-                        $person = [
-                            "first_name" => $first_name,
-                            "last_name" => $last_name,
-                            "birthdate" => $birthdate,
-                            "genre" => $genre,
-                            "isActor" => $isActor,
-                            "isDirector" => $isDirector,
-                            "portrait" => $portrait
-                        ];
-
-                        // $ctrlCinema->createPerson($person);
-
-                        // if (isset($portrait) && $portrait != null) {
-                        //     $ctrlCinema->updatePortrait($id, $portrait);
-                        // }
-
-                        // $_SESSION['message'] = "<p class='text-success fw-semibold fs-4'>Person successfully modified</p>";
-                    } else {
-                        // $_SESSION['message'] = "<p class='text-danger fw-semibold fs-4'>Person has to be either an actor, or a director<p>";
-                    }
-                } else {
-
-                    // $_SESSION['message'] = "<p class='text-danger fw-semibold fs-4'>Error<p>";
-                }
-            }
+            $ctrlCinema->createPersonDashboard();
 
             break;
     }

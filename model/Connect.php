720f652244f98c00738c3af60a504d1ed138fe7a
Charly.ducournau@outlook.fr <Charly.ducournau@outlook.fr>
2024-02-15 15:59:24 +0100

    docker (3h sur une majuscule ^^)

--- model/Connect.php
+++ model/Connect.php
@@ -3,24 +3,26 @@
 namespace Model;
 
 abstract class Connect {
-    const HOST = 'localhost:3307';
-    const DB = 'cinema_v2';
-    const USER = 'root';
-    const PASS = '';
+
+    const HOST = "db";
+    const DB = "cinema_v2";
+    const USER = "root";
+    const PASS = "";
 
     public static function dbConnect() {
+        try {
+            phpinfo();die;
+            return new \PDO(
+                    "mysql:host=" . self::HOST . ";dbname=" . self::DB . ";charset=utf8", self::USER, self::PASS);
 
-        // var_dump(new \PDO('mysql:host='.self::HOST.'dbname='.self::DB.';charset=utf8',self::USER,self::PASS));die;
-
-
-        phpinfo();die;
-        // try {
-        //     return new PDO('mysql:host='.self::HOST.'dbname='.self::DB.';charset=utf8',self::USER,self::PASS);
-        // } catch(\PDOException $ex) {
-        //     return $ex->getMessage();
-        // }
+        } catch(\PDOException $ex) {
+            return $ex->getMessage();
 
         }
     }
+
+
+
+}
 
 ?>

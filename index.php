6ded4d11404f4f0bd2b0aa845a75f3e070ae9e7c
Charly.ducournau@outlook.fr <Charly.ducournau@outlook.fr>
2024-02-15 15:08:14 +0100

    docker

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 use Controller\CinemaController;
 
 spl_autoload_register(function ($class_name) {
-    include $class_name . '.php';
+    require str_replace("\\", DIRECTORY_SEPARATOR, $class_name) . '.php';
 });
 
 $ctrlCinema = new CinemaController();
